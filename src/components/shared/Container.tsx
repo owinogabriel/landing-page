aa5e68280d4a0f2a6506065a409261f2a6255e25
Leon Gabriel <g73346920@gmail.com>
2025-05-08 19:49:36 +0300

    modified

--- src/components/shared/Container.tsx
+++ src/components/shared/Container.tsx
@@ -7,8 +7,8 @@ interface ContainerProps {
 
 export const Container = ({ children, className = "" }: ContainerProps) => {
   return (
-   <div className={`mx-auto max-w-7xl w-full px-5 sm:px-14 lg:px-5 ${className}`}>
-    {children}
-   </div>
+    <div className={`mx-auto max-w-7xl w-full px-5 sm:px-14 lg:px-5 ${className}`}>
+      {children}
+    </div>
   )
 }
