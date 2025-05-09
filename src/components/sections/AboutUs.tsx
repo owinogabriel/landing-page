351752a0ae0dce20ea3ed05bef56d037c7d8c886
Leon Gabriel <g73346920@gmail.com>
2025-05-09 15:46:49 +0300

    Chored:Functional component that tells more about the service

---
+++ src/components/sections/AboutUs.tsx
@@ -0,0 +1,37 @@
+import { Container } from "../shared/Container"
+import { Paragraph } from "../shared/Paragraph"
+import { Title } from "../shared/Title"
+
+
+
+export const AboutUs = () => {
+  return (
+    <section id="about-us">
+      <Container className="flex flex-col md:flex-row gap-10 lg:gap-12 items-center">
+        <div className="w-full md:w-5/12 lg:w-1/2">
+          <div className="w-full h-80 sm:h-96 relative">
+            <img
+              src="https://images.pexels.com/photos/8728381/pexels-photo-8728381.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              className="w-full h-full object-cover rounded-3xl shadow-lg relative z-10"
+              alt="About Our Mission"
+            />
+          </div>
+        </div>
+        <div className="w-full md:w-7/12 lg:w-1/2 flex flex-col">
+          <Title>About AI Solution</Title>
+          <Paragraph>
+            We are on a mission to empower businesses woth transformative AI
+            technology. Our team of eperts combines industries-leading research
+            with innovative algorithms to deliver a platform that adapts to your
+            unique needs. Join us and lead the digital revolution in your industry.
+          </Paragraph>
+
+          <div>
+            
+          </div>
+        </div>
+      </Container>
+
+    </section>
+  )
+}
