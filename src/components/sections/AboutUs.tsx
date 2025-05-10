971aaf33c353e601bc9b2c2f2abc4c337877ed1b
Leon Gabriel <g73346920@gmail.com>
2025-05-10 17:41:15 +0300

    Chored:Component to show mor about the AI solution

--- src/components/sections/AboutUs.tsx
+++ src/components/sections/AboutUs.tsx
@@ -1,3 +1,4 @@
+import { Info } from "../cards/info"
 import { Container } from "../shared/Container"
 import { Paragraph } from "../shared/Paragraph"
 import { Title } from "../shared/Title"
@@ -26,8 +27,47 @@ export const AboutUs = () => {
             unique needs. Join us and lead the digital revolution in your industry.
           </Paragraph>
 
-          <div>
-            
+          <div className="pt-8 grid grid-cols-1 sm:grid-cols-2 gap-4 max-w-3xl">
+          <Info
+              title="Mission"
+              description="Our mission is to use AI for the good of humanity and the planet."
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-4 h-4 sm:w-5 sm:h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
+                />
+              </svg>
+            </Info>
+            <Info
+              title="Vision"
+              description="Our vision is to drive business innovation and growth."
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-4 h-4 sm:w-5 sm:h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
+                />
+              </svg>
+            </Info>
           </div>
         </div>
       </Container>
